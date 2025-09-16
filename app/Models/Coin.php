c7e728ce5a9d597e6f8e066549e19d0ff91c2bdc
Fabio Costa <costantinofabio84@gmail.com>
2025-09-17 00:58:09 +0200

    Aggiunto use e commenti nei Models

--- app/Models/Coin.php
+++ app/Models/Coin.php
@@ -2,10 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Coin extends Model
 {
+    use HasFactory;
+
+    // Relazione: una coin ha molti prezzi
     public function prices()
     {
         return $this->hasMany(Price::class);
--- app/Models/Price.php
+++ app/Models/Price.php
@@ -2,10 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Price extends Model
 {
+    use HasFactory;
+
+    // Relazione: un prezzo appartiene a una coin
     public function Coins()
     {
         return $this->belongsTo(Coin::class);
