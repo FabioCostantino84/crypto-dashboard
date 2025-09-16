62038f1aa2f9cc36abb4584a0dcb5354b719fba1
Fabio Costa <costantinofabio84@gmail.com>
2025-09-17 01:25:18 +0200

    Creato il service CoinGeckoService per gestire separatamente le chiamate API

---
+++ app/Services/CoinGeckoService.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Services;
+
+class CoinGeckoService{
+
+    public function getCoinsList()
+    {
+        // qui devo fare la chimata http alla API di CoinGecko
+    }
+
+    public function getCoinPrices()
+    {
+        // qui devo fare la chimata http alla API di CoinGecko per recuperare i valori di una singola Coin
+    }
+}
